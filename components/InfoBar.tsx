f742f737b3337a5358850e9c7095b62779c4998f
scroobius-pip <sim04ful@gmail.com>
2020-08-25 13:45:59 +0100

    info bar icon too small

--- components/InfoBar.tsx
+++ components/InfoBar.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Icons, { IconNames } from '../icons'
+import React from "react";
+import Icons, { IconNames } from "../icons";
 
 interface Props {
-    className: string
-    icon: IconNames
-    text: string
+  className: string;
+  icon: IconNames;
+  text: string;
 }
 
 export default ({ className, icon, text }: Props) => {
-    const Icon = Icons[icon]
-    return <div className={`flex items-center ${className}`}>
-        <Icon className='w-4 h-4 inline fill-current mr-2 align-middle' />
-        <span className='font-pop '>{text}</span>
-    </div>
-}
+  const Icon = Icons[icon];
+  return <div className={` ${className}`}>
+    <Icon className="w-6 h-6 inline fill-current mr-3" />
+    <span className="font-pop ">{text}</span>
+  </div>;
+};
