78d66f39dc164a2fc1da24fc6d01c45b9b083c5f
scroobius-pip <sim04ful@gmail.com>
2020-09-07 13:56:52 +0100

    added sentry

--- next.config.js
+++ next.config.js
@@ -1,3 +1,70 @@
+//@ts-nocheck
 // next.config.js
-const withImages = require('next-images')
-module.exports = withImages()
+const withSourceMaps = require("@zeit/next-source-maps")();
+const withImages = require("next-images");
+
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
+const {
+  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
+  SENTRY_ORG,
+  SENTRY_PROJECT,
+  SENTRY_AUTH_TOKEN,
+  NODE_ENV,
+  VERCEL_GITHUB_COMMIT_SHA,
+  VERCEL_GITLAB_COMMIT_SHA,
+  VERCEL_BITBUCKET_COMMIT_SHA,
+} = process.env;
+
+const COMMIT_SHA = VERCEL_GITHUB_COMMIT_SHA ||
+  VERCEL_GITLAB_COMMIT_SHA ||
+  VERCEL_BITBUCKET_COMMIT_SHA;
+
+process.env.SENTRY_DSN = SENTRY_DSN;
+
+module.exports = withImages(withSourceMaps({
+  webpack: (config, options) => {
+    // In `pages/_app.js`, Sentry is imported from @sentry/browser. While
+    // @sentry/node will run in a Node.js environment. @sentry/node will use
+    // Node.js-only APIs to catch even more unhandled exceptions.
+    //
+    // This works well when Next.js is SSRing your page on a server with
+    // Node.js, but it is not what we want when your client-side bundle is being
+    // executed by a browser.
+    //
+    // Luckily, Next.js will call this webpack function twice, once for the
+    // server and once for the client. Read more:
+    // https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+    //
+    // So ask Webpack to replace @sentry/node imports with @sentry/browser when
+    // building the browser's bundle
+    if (!options.isServer) {
+      config.resolve.alias["@sentry/node"] = "@sentry/browser";
+    }
+
+    // When all the Sentry configuration env variables are available/configured
+    // The Sentry webpack plugin gets pushed to the webpack plugins to build
+    // and upload the source maps to sentry.
+    // This is an alternative to manually uploading the source maps
+    // Note: This is disabled in development mode.
+    if (
+      SENTRY_DSN &&
+      SENTRY_ORG &&
+      SENTRY_PROJECT &&
+      SENTRY_AUTH_TOKEN &&
+      COMMIT_SHA &&
+      NODE_ENV === "production"
+    ) {
+      config.plugins.push(
+        new SentryWebpackPlugin({
+          include: ".next",
+          ignore: ["node_modules"],
+          urlPrefix: "~/_next",
+          release: COMMIT_SHA,
+        }),
+      );
+    }
+
+    return config;
+  },
+}));
+// module.exports =
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,53 +1,124 @@
-import '../css/fonts.css'
-import '../css/tailwind.css'
-import 'react-responsive-modal/styles.css'
-import 'react-image-lightbox/style.css'
-import { Provider } from 'next-auth/client'
-import Head from 'next/head'
+import "../css/fonts.css";
+import "../css/tailwind.css";
+import "react-responsive-modal/styles.css";
+import "react-image-lightbox/style.css";
+import { Provider } from "next-auth/client";
+import Head from "next/head";
+import * as Sentry from "@sentry/node";
 
-export default function MyApp({ Component, pageProps }) {
+if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
+  Sentry.init({
+    enabled: process.env.NODE_ENV === "production",
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  });
+}
 
-  const { session } = pageProps
+export default function MyApp({ Component, pageProps, err }) {
+  const { session } = pageProps;
 
   return <Provider session={session}>
     <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-175017430-1"></script>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-175017430-1"
+      >
+      </script>
       <script dangerouslySetInnerHTML={googleAnalyticsCode} />
 
       <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
       <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
       <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
       <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-      <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-      <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-      <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-      <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href="/apple-icon-114x114.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/apple-icon-120x120.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/apple-icon-144x144.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/apple-icon-152x152.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-icon-180x180.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/android-icon-192x192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/favicon-96x96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
       <link rel="manifest" href="/manifest.json" />
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
       <meta name="theme-color" content="#ffffff" />
-      <meta name="title" content="Zanga - Advertise real estate and earn money" />
-      <meta name="description" content="Advertise your property for free on our platform, and reach more audience than traditional channels" />
+      <meta
+        name="title"
+        content="Zanga - Advertise real estate and earn money"
+      />
+      <meta
+        name="description"
+        content="Advertise your property for free on our platform, and reach more audience than traditional channels"
+      />
 
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://myzanga.com/" />
-      <meta property="og:title" content="Zanga - Advertise real estate and earn money" />
-      <meta property="og:description" content="Advertise your property for free on our platform, and reach more audience than traditional channels" />
+      <meta
+        property="og:title"
+        content="Zanga - Advertise real estate and earn money"
+      />
+      <meta
+        property="og:description"
+        content="Advertise your property for free on our platform, and reach more audience than traditional channels"
+      />
       <meta property="og:image" content="https://myzanga.com/zanga-logo.png" />
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://myzanga.com/" />
-      <meta property="twitter:title" content="Zanga - Advertise real estate and earn money" />
-      <meta property="twitter:description" content="Advertise your property for free on our platform, and reach more audience than traditional channels" />
-      <meta property="twitter:image" content="https://myzanga.com/zanga-logo.png" />
-
+      <meta
+        property="twitter:title"
+        content="Zanga - Advertise real estate and earn money"
+      />
+      <meta
+        property="twitter:description"
+        content="Advertise your property for free on our platform, and reach more audience than traditional channels"
+      />
+      <meta
+        property="twitter:image"
+        content="https://myzanga.com/zanga-logo.png"
+      />
     </Head>
-    <Component key='the-app' {...pageProps} />
+    <Component key="the-app" {...pageProps} err={err} />
     <style jsx>
       {`
   body {
@@ -55,9 +126,8 @@ export default function MyApp({ Component, pageProps }) {
   }
   `}
     </style>
-  </Provider>
+  </Provider>;
 }
-
 
 const googleAnalyticsCode = {
   __html: `
@@ -66,5 +136,5 @@ const googleAnalyticsCode = {
   gtag('js', new Date());
 
   gtag('config', 'UA-175017430-1');
-  `
-}
+  `,
+};
